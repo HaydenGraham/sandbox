8ceb335e21f1798aee487719b6b37db9f07cbc45
HaydenGraham <hayden.graham@my.jcu.edu.au>
2017-03-20 00:19:58 +1000

    add function organizing and basic error handling

--- oddName.py
+++ oddName.py
@@ -1,6 +1,23 @@
 """ Hayden """
-name = str(input("inster name"))
-while len(name) == 0:
-    name=input("insert name")
-for i in range(0, len(name), 2):
-    print(name[i])
+def main():
+
+    get_name()
+
+
+def get_name():
+    try:
+     name = str(input("Insert name:   "))
+     while len(name) == 0:
+         name = input("Insert name:  ")
+     freq = int(input("What frequency of letters would you like to print? "))
+     loop(name,freq)
+    except ValueError:
+        print("Enter a valid number")
+
+
+def loop(name,freq):
+    for i in range(0, len(name), freq):
+        print(name[i])
+
+
+main()
